c2ce671fa6cf14fe4b1bcac0bd41c0fd1c6ad9c7
vsalvis <salvisbergvera@gmail.com>
2013-01-10 00:08:53 +0100

    Added generic map with function argument to DSL.

--- ScalaStreams/src/main/scala/dsl/DSL.scala
+++ ScalaStreams/src/main/scala/dsl/DSL.scala
@@ -1,5 +1,6 @@
 package dsl
 
+import scala.virtualization.lms.common
 import scala.virtualization.lms.common._
 import scala.virtualization.lms.common.Functions
 import streams._
@@ -10,20 +11,20 @@ trait StreamDSL { this: Base =>
   // Concepts
   type DoubleStream
   def DoubleStream_scale(s: Rep[DoubleStream], k: Rep[Double]): Rep[DoubleStream]
-//  def DoubleStream_map(s: Rep[DoubleStream], f: Rep[Double] => Rep[Double]): Rep[DoubleStream]
+  def DoubleStream_map(s: Rep[DoubleStream], f: Rep[Double] => Rep[Double]): Rep[DoubleStream]
   
   // Concrete syntax
   final def infix_*(s: Rep[DoubleStream], k: Rep[Double]): Rep[DoubleStream] = DoubleStream_scale(s, k)
 }
 
 // Intermediate representation
-trait StreamDSLExp extends StreamDSL with FunctionsExp { this: BaseExp =>
+trait StreamDSLExp extends StreamDSL with FunctionsExp with MathOpsExp { this: BaseExp =>
 
   case class DoubleStreamScale(s: Exp[DoubleStream], k: Exp[Double]) extends Def[DoubleStream]
   override def DoubleStream_scale(s: Exp[DoubleStream], k: Exp[Double]) = DoubleStreamScale(s, k)
 
-//  case class DoubleStreamMap(s: Exp[DoubleStream], f: Exp[Double => Double]) extends Def[DoubleStream]
-//  override def DoubleStream_map(s: Exp[DoubleStream], f: Exp[Double => Double]) = DoubleStreamMap(s, f)
+  case class DoubleStreamMap(s: Exp[DoubleStream], f: Exp[Double => Double]) extends Def[DoubleStream]
+  override def DoubleStream_map(s: Exp[DoubleStream], f: Exp[Double] => Exp[Double]) = DoubleStreamMap(s, doLambdaDef(f))
 
   override type DoubleStream = Stream[Double, Double]
 }
@@ -47,32 +48,32 @@ trait ScalaGenStreamDSL extends ScalaGenBase with ScalaGenFunctions {
     case DoubleStreamScale(s, k) => {
       emitValDef(sym, quote(s) + ".map(x => x * " + quote(k) + ")")
     }
-//    case DoubleStreamMap(s, f) => {
-//      emitValDef(sym, quote(s) + ".map(" + quote(f) + ")")
-//    }
+    case DoubleStreamMap(s, f) => {
+      emitValDef(sym, quote(s) + ".map(" + quote(f) + ")")
+    }
     case _ => super.emitNode(sym, node)
   }
 
 }
 
 // Usage
-trait Prog { this: Base with StreamDSL =>
+trait Prog { this: Base with StreamDSL with MathOps =>
 
   def f(s: Rep[DoubleStream]): Rep[DoubleStream] = s * unit(42.0)
 
   def g(s: Rep[DoubleStream]): Rep[DoubleStream] = s * unit(1.0)
   
-//  def h(s: Rep[DoubleStream]): Rep[DoubleStream] = DoubleStream_map(s, {(x: Double) => scala.math.pow(2.0, x)})
+  def h(s: Rep[DoubleStream]): Rep[DoubleStream] = DoubleStream_map(s, {(x: Rep[Double]) => Math.pow(unit(2.0), x)})
 
 }
 
 object Usage extends App {
 
   val concreteProg = new Prog with EffectExp with StreamDSLExp with StreamDSLOpt with CompileScala { self =>
-    override val codegen = new ScalaGenEffect with ScalaGenStreamDSL { val IR: self.type = self }
+    override val codegen = new ScalaGenEffect with ScalaGenStreamDSL with ScalaGenMathOps { val IR: self.type = self }
     codegen.emitSource(f, "F", new java.io.PrintWriter(System.out))
     codegen.emitSource(g, "G", new java.io.PrintWriter(System.out))
-//    codegen.emitSource(h, "H", new java.io.PrintWriter(System.out))
+    codegen.emitSource(h, "H", new java.io.PrintWriter(System.out))
   }
 
   val f = concreteProg.compile(concreteProg.f)
@@ -81,12 +82,13 @@ object Usage extends App {
 
   println
   val g = concreteProg.compile(concreteProg.g)
-  println("scaling 1.0 :: 2.0 :: 3.0 :: Nil by a factor of 1.0: ")
+  println("\nscaling 1.0 :: 2.0 :: 3.0 :: Nil by a factor of 1.0: ")
   new streams.ListInput(1.0 :: 2.0 :: 3.0 :: Nil, g(Stream[Double]) print)
 
-//  val h = concreteProg.compile(concreteProg.h)
-//  println("\n2^x for x = 1.0 :: 2.0 :: 3.0 :: Nil: ")
-//  new streams.ListInput(1.0 :: 2.0 :: 3.0 :: Nil, h(Stream[Double]) print)
+  println
+  val h = concreteProg.compile(concreteProg.h)
+  println("\n2^x for x = 1.0 :: 2.0 :: 3.0 :: Nil: ")
+  new streams.ListInput(1.0 :: 2.0 :: 3.0 :: Nil, h(Stream[Double]) print)
 
   println
 }
