f7853ac00739acea4f20a7f1c806e86d2d8e82e4
vsalvis <salvisbergvera@gmail.com>
2013-02-28 13:16:35 +0100

    Generic DSL using Stream[A,B]. Build.sbt with LMS and ScalaTest.

--- ScalaStreams/build.sbt
+++ ScalaStreams/build.sbt
@@ -2,7 +2,7 @@ name := "streams"
 
 version := "1.0"
 
-scalaVersion := "2.10.0-M7"
+scalaVersion := "2.10.0"
 //M7 works for lms
 //RC5 works for scalameter
 
@@ -15,7 +15,22 @@ resolvers += ScalaToolsSnapshots
 
 libraryDependencies += "com.github.axel22" %% "scalameter" % "0.2"
 
-libraryDependencies += "EPFL" %% "lms" % "0.3-SNAPSHOT"
+libraryDependencies += "org.scala-lang.virtualized" % "scala-library" % "2.10.0"
+
+libraryDependencies += "org.scala-lang.virtualized" % "scala-compiler" % "2.10.0"
+
+libraryDependencies += "org.scala-lang" % "scala-actors" % "2.10.0" // for ScalaTest
+
+libraryDependencies ++= Seq(
+    "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test",
+    "EPFL" % "lms_2.10.0" % "0.3-SNAPSHOT")
+
+//libraryDependencies += "EPFL" %% "lms" % "0.3-SNAPSHOT"
+//libraryDependencies += "EPFL" % "lms_2.10.0" % "0.3-SNAPSHOT"
+
+//libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+//http://mvnrepository.com/artifact/org.scalatest/scalatest_2.10.0-M7/1.9-2.10.0-M7-B1
+//libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test"
 
 scalacOptions += "-Yvirtualize"
 
--- ScalaStreams/src/main/scala/dsl/DSL.scala
+++ ScalaStreams/src/main/scala/dsl/DSL.scala
@@ -5,46 +5,78 @@ import scala.virtualization.lms.common._
 import scala.virtualization.lms.common.Functions
 import streams._
 
+////StreamOps
+//abstract class StreamOp[Rep[A]] {
+//  def onData(data: Rep[A])
+//}
+//class MapOp[Rep[A], Rep[B]](f: Rep[A] => Rep[B], next: StreamOp[Rep[B]]) extends StreamOp[Rep[A]] {
+//  def onData(data: Rep[A]) = next.onData(f(data))
+//}
+//class PrintlnOp[Rep[A]] extends StreamOp[Rep[A]] {
+//  def onData(data: Rep[A]) = println(data)
+//}
+//class ListInput[Rep[A]](input: List[A], stream: StreamOp[Rep[A]]) {
+//  input foreach stream.onData
+//}
+//
+//
+////API
+//object Stream {
+//  def apply[A] = new Stream[A,A] {
+//    def into(out: StreamOp[A]): StreamOp[A] = out
+//  }
+//}
+//
+//abstract class Stream[A,B] { self =>
+//  def into(out: StreamOp[B]): StreamOp[A]
+//  def into[C](next: Stream[B, C]): Stream[A, C] = new Stream[A, C] {
+//    def into(out: StreamOp[C]) = self.into(next.into(out))
+//  }
+//  
+//  def print = into(new PrintlnOp[B]())
+//  
+//  def map[C](f: B => C) = new Stream[A,C] {
+//    def into(out: StreamOp[C]) = self.into(new MapOp(f, out))
+//  }
+//}
+
 // Concepts and concrete syntax
 trait StreamDSL { this: Base =>
 
   // Concepts
-  type DoubleStream
-  def scale(s: Rep[DoubleStream], k: Rep[Double]): Rep[DoubleStream]
-  def map(s: Rep[DoubleStream], f: Rep[Double] => Rep[Double]): Rep[DoubleStream]
+  def scale[A:Manifest](s: Rep[Stream[A,Double]], k: Rep[Double]): Rep[Stream[A,Double]]
+  def map[A:Manifest, B:Manifest, C:Manifest](s: Rep[Stream[A,B]], f: Rep[B] => Rep[C]): Rep[Stream[A,C]]
   
   // Concrete syntax
-  final def infix_*(s: Rep[DoubleStream], k: Rep[Double]): Rep[DoubleStream] = scale(s, k)
+  final def infix_*[A: Manifest](s: Rep[Stream[A,Double]], k: Rep[Double]): Rep[Stream[A,Double]] = scale[A](s, k)
 }
 
 // Intermediate representation
-trait StreamDSLExp extends StreamDSL with FunctionsExp with MathOpsExp with NumericOpsExp with NumericOpsExpOpt { this: BaseExp =>
+trait StreamDSLExp extends StreamDSL with FunctionsExp with MathOpsExp with NumericOpsExp with NumericOpsExpOpt with StringOpsExp { this: BaseExp =>
 
-  case class Scale(s: Exp[DoubleStream], k: Exp[Double]) extends Def[DoubleStream]
-  override def scale(s: Exp[DoubleStream], k: Exp[Double]) = Scale(s, k)
+  case class Scale[A](s: Exp[Stream[A,Double]], k: Exp[Double]) extends Def[Stream[A,Double]]
+  override def scale[A:Manifest](s: Exp[Stream[A,Double]], k: Exp[Double]) = Scale[A](s, k)
 
-  case class Map(s: Exp[DoubleStream], f: Exp[Double => Double]) extends Def[DoubleStream]
-  override def map(s: Exp[DoubleStream], f: Exp[Double] => Exp[Double]) = Map(s, doLambdaDef(f))
+  case class Map[A,B,C](s: Exp[Stream[A,B]], f: Exp[B => C]) extends Def[Stream[A,C]]
+  override def map[A:Manifest, B:Manifest, C:Manifest](s: Exp[Stream[A,B]], f: Exp[B] => Exp[C]) = Map[A,B,C](s, doLambdaDef(f))
 
-  override type DoubleStream = Stream[Double, Double]
-  
 }
 
 // Optimizations working on the intermediate representation
 trait StreamDSLOpt extends StreamDSLExp { this: BaseExp =>
 
-  override def scale(s: Exp[DoubleStream], k: Exp[Double]) = k match {
+  override def scale[A:Manifest](s: Exp[Stream[A,Double]], k: Exp[Double]) = k match {
     case Const(1.0) => s
     case _ => s match {
-      case Def(Scale(s1, k1)) => super.scale(s1, k * k1)
-      case Def(Map(s1, f)) => super.map(s1, {x => k * f(x)})
+      case Def(Scale(s1: Exp[Stream[A,Double]], k1: Exp[Double])) => super.scale[A](s1, k * k1)
+//      case Def(Map(s1, f)) => super.map(s1, {x: A => k * f(x)})
       case _ => super.scale(s, k)
     }
   }
   
-  override def map(s: Exp[DoubleStream], f: Exp[Double] => Exp[Double]) = s match {
-    case Def(Scale(s1, k)) => super.map(s1, {x => f(k * x)})
-    case Def(Map(s1, f1)) => super.map(s1, {x => f(f1(x))})
+  override def map[A:Manifest, B:Manifest, C:Manifest](s: Exp[Stream[A,B]], f: Exp[B] => Exp[C]) = s match {
+//    case Def(Scale(s1, k)) => super.map(s1, {x => f(k * x)})
+//    case Def(Map(s1, Def(Lambda(f1,_,_)))) => super.map(s1, {x => f(f1(x))})
     case _ => super.map(s, f)
   }
 
@@ -68,27 +100,28 @@ trait ScalaGenStreamDSL extends ScalaGenBase with ScalaGenFunctions {
 }
 
 // Usage
-trait Prog { this: Base with StreamDSL with MathOps with NumericOps =>
+trait Prog { this: Base with StreamDSL with MathOps with NumericOps with StringOps =>
 
-  def f(s: Rep[DoubleStream]): Rep[DoubleStream] = s * unit(42.0)
+  def f(s: Rep[Stream[Int,Double]]): Rep[Stream[Int,Double]] = s * unit(42.0)
 
-  def g(s: Rep[DoubleStream]): Rep[DoubleStream] = s * unit(1.0)
+  def g(s: Rep[Stream[Int,Double]]): Rep[Stream[Int,Double]] = s * unit(1.0)
   
-  def h(s: Rep[DoubleStream]): Rep[DoubleStream] = map(s, {(x: Rep[Double]) => Math.pow(unit(2.0), x)})
+  def h(s: Rep[Stream[Double,Double]]): Rep[Stream[Double,Double]] = map[Double,Double,Double](s, {(x: Rep[Double]) => Math.pow(unit(2.0), x)})
   
-  def test(s: Rep[DoubleStream]): Rep[DoubleStream] = 
+  def test(s: Rep[Stream[Double,Double]]): Rep[Stream[Double,Double]] = 
     map(map(s, {(x: Rep[Double]) => Math.pow(unit(2.0), x)}),
         {(x: Rep[Double]) => x + unit(3.0)})
   
-  def f1(s: Rep[DoubleStream]): Rep[DoubleStream] = s * unit(42.0) * unit(2.0) * unit(3.0)
-  def f2(s: Rep[DoubleStream]): Rep[DoubleStream] = map(s, {(x: Rep[Double]) => Math.pow(unit(2.0), x)}) * unit(2.0) * unit(3.0)
-  def f3(s: Rep[DoubleStream]): Rep[DoubleStream] = map(s * unit(42.0) * unit(3.0), {(x: Rep[Double]) => Math.pow(unit(2.0), x)}) * unit(2.0)
+  def f1(s: Rep[Stream[Double,Double]]): Rep[Stream[Double,Double]] = s * unit(42.0) * unit(2.0) * unit(3.0)
+  def f2(s: Rep[Stream[Double,Double]]): Rep[Stream[Double,Double]] = map(s, {(x: Rep[Double]) => Math.pow(unit(2.0), x)}) * unit(2.0) * unit(3.0)
+  def f3(s: Rep[Stream[Double,Double]]): Rep[Stream[Double,Double]] = map(s * unit(42.0) * unit(3.0), {(x: Rep[Double]) => Math.pow(unit(2.0), x)}) * unit(2.0)
+  def f4(s: Rep[Stream[Int,Double]]): Rep[Stream[Int,String]] = map[Int,Double,String](s, {(x: Rep[Double]) => "'" + x + "'"})
 
 }
 
 object Usage extends App {
   val concreteProg = new Prog with EffectExp with StreamDSLExp with StreamDSLOpt with CompileScala { self =>
-    override val codegen = new ScalaGenEffect with ScalaGenStreamDSL with ScalaGenMathOps with ScalaGenNumericOps { val IR: self.type = self }
+    override val codegen = new ScalaGenEffect with ScalaGenStreamDSL with ScalaGenMathOps with ScalaGenNumericOps with ScalaGenStringOps { val IR: self.type = self }
     codegen.emitSource(f, "F", new java.io.PrintWriter(System.out))
     codegen.emitSource(g, "G", new java.io.PrintWriter(System.out))
     codegen.emitSource(h, "H", new java.io.PrintWriter(System.out))
@@ -96,21 +129,27 @@ object Usage extends App {
     codegen.emitSource(f1, "F1", new java.io.PrintWriter(System.out))
     codegen.emitSource(f2, "F2", new java.io.PrintWriter(System.out))
     codegen.emitSource(f3, "F3", new java.io.PrintWriter(System.out))
+    codegen.emitSource(f4, "F4", new java.io.PrintWriter(System.out))
   }
 
   val f = concreteProg.compile(concreteProg.f)
   println("scaling 1.0 :: 2.0 :: 3.0 :: Nil by a factor of 42: ")
-  new streams.ListInput(1.0 :: 2.0 :: 3.0 :: Nil, f(Stream[Double]) print)
+  new streams.ListInput(1 :: 2 :: 3 :: Nil, f(Stream[Int] map({x: Int => 2.5 * x})) print)
 
   println
   val g = concreteProg.compile(concreteProg.g)
   println("\nscaling 1.0 :: 2.0 :: 3.0 :: Nil by a factor of 1.0: ")
-  new streams.ListInput(1.0 :: 2.0 :: 3.0 :: Nil, g(Stream[Double]) print)
+  new streams.ListInput(1 :: 2 :: 3 :: Nil, g(Stream[Int] map({x: Int => 2.5 * x})) print)
 
   println
   val h = concreteProg.compile(concreteProg.h)
   println("\n2^x for x = 1.0 :: 2.0 :: 3.0 :: Nil: ")
   new streams.ListInput(1.0 :: 2.0 :: 3.0 :: Nil, h(Stream[Double]) print)
+
+  println
+  val f4 = concreteProg.compile(concreteProg.f4)
+  println("\n'2.5*x' for x = 1 :: 2 :: 3 :: Nil: ")
+  new streams.ListInput(1 :: 2 :: 3 :: Nil, f4(Stream[Int] map({x: Int => 2.5 * x})) print)
 
   println
   val test = concreteProg.compile(concreteProg.test)
@@ -121,35 +160,4 @@ object Usage extends App {
   println
 }
 
-// Without optimization:
-class Test extends (Stream[Double,Double]=>Stream[Double,Double]) {
-  def apply(x0:Stream[Double,Double]): Stream[Double,Double] = {
-    val x3 = {x1: (Double) => 
-      val x2 = Math.pow(2.0,x1)
-      x2: Double
-    }
-    val x4 = x0.map(x3)  // MapOp
-      val x7 = {x5: (Double) => 
-      val x6 = x5 + 3.0
-      x6: Double
-    }
-    val x8 = x4.map(x7)  // MapOp
-    x8
-  }
-}
-// With optimization:
-class TestOpt extends (Stream[Double,Double]=>Stream[Double,Double]) {
-  def apply(x0:Stream[Double,Double]): Stream[Double,Double] = {
-    val x3 = {x1: (Double) => 
-      val x2 = Math.pow(2.0,x1)
-      x2: Double
-    }
-    val x8 = {x5: (Double) => 
-      val x6 = x3(x5)
-      val x7 = x6 + 3.0
-      x7: Double
-    }
-    val x9 = x0.map(x8)  // MapOp
-    x9
-  }
-}
+
